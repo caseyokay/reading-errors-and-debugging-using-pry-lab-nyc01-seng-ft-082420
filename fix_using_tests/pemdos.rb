f48dbb138a19f175f56b241952b029d4e64c2679
Casey B <caseyoksana@gmail.com>
2020-08-16 17:30:43 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -9,16 +9,16 @@
 #   end
 # end
 
-def snake_it_up(string)
-  if string[0] == "s"
-    new_string = string[0]
-    10.times do
-      new_string
-    end
-    new_string + string
-  else
-    string
-  end
-end
+# def snake_it_up(string)
+#   if string[0] == "s"
+#     new_string = string[0]
+#     10.times do
+#       new_string
+#     end
+#     new_string + string
+#   else
+#     string
+#   end
+# end
 
 snake_it_up("surprise!")
