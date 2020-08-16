90a4aa470124126e461ec62297b1fe554e7e5914
caseyokay <61754743+caseyokay@users.noreply.github.com>
2020-08-16 13:31:40 -0400

    Update pemdos.rb

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,7 +1,19 @@
+# def snake_it_up(string)
+#   if string[0] == "s"
+#   's' * 10 + string
+#   else
+#   string
+#   end
+# end
+
 def snake_it_up(string)
   if string[0] == "s"
-  's' * 10 + string
+    new_string = ''
+    10.times do
+      new_string += 's'
+    end
+    new_string + string
   else
-  string
+    string
   end
 end
